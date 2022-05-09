c61bf661ba0bee1572191d937356d5ed706fbf17
aiyc <1432803776@qq.com>
2022-05-09 09:57:08 +0800

    update

--- bornforthis/.vuepress/sidebar.ts
+++ bornforthis/.vuepress/sidebar.ts
@@ -54,7 +54,7 @@ export default sidebar({
       text: "Python 基础入门课程计划",
 	  // 可选的, 设置分组是否可以折叠，默认值是 false,
 	  collapsable: true,
-	  link: "../../../onepython.md",
+	  link: "../../../onepython",
       children: [
         "q1",
 		"hw_word_game",
