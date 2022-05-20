83189a0c8b6d6307b97c93972a5431399f4d1b3b
AndersonHJB <cleland1432803776@icloud.com>
2022-05-20 07:11:02 +0000

    Deploying to gh-pages from @ BornforthisHJB/BornforthisHJB.github.io@a74046395c3a6198d7041942e7fe60f3c14c3e49 🚀

---
+++ js/myjs.js
@@ -0,0 +1,29 @@
+// <!--Start of Tawk.to Script-->
+// https://www.tawk.to/
+
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/62873a57b0d10b6f3e732a51/1g3g3nq35';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+
+
+// https://app.chatra.io/
+// <!-- Chatra {literal} -->
+{/* <script> */}
+    (function(d, w, c) {
+        w.ChatraID = 'EAfok66ZyJmrAiWpC';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+// </script>
+// <!-- /Chatra {/literal} -->
