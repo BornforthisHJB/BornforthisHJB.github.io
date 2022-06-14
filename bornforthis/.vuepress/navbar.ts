b9d406a5749b1939a7232a4920883ce5e28f9273
aiyc <1432803776@qq.com>
2022-06-15 00:18:02 +0800

    update

--- bornforthis/.vuepress/navbar.ts
+++ bornforthis/.vuepress/navbar.ts
@@ -77,10 +77,17 @@ export default navbar([
           { text: "Vue", icon: "vue", link: "https://v2.vuepress.vuejs.org/zh/"},
           { text: "主题", icon: "vue", link: "https://vuepress-theme-hope.github.io/v2/zh/"},
           { text: "Waline", icon: "waline", link: "https://waline.js.org/"},
-          { text: "Unsplash", icon: "pic", link: "https://unsplash.com/"},
           { text: "iconfont", link: "https://www.iconfont.cn/"},
           { text: "Leancloud", link: "https://leancloud.app/"},
         ],
+      },
+      { 
+        text: "审美",
+        icon: "eye",
+        children: [
+          { text: "Unsplash", icon: "pic", link: "https://unsplash.com/"},
+          { text: "Dribbble", icon: "emoji", link: "https://dribbble.com/"},
+        ]
       }
     ],
   },
