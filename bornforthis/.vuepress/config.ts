5480e416dff4d887af5f7c3a5a2c1062c6246b97
aiyc <1432803776@qq.com>
2022-06-14 10:42:00 +0800

    update

--- bornforthis/.vuepress/config.ts
+++ bornforthis/.vuepress/config.ts
@@ -6,7 +6,6 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "AI悦创",
   description: "AI悦创,Python,编程一对一,C++,Java,AI,人工智能,黄家宝",
-  // searchPlugin: true,
   base: "/",
   head: [
     [
