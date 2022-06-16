419b58c0748d96fe819321ad6a682e89bec35a42
aiyc <1432803776@qq.com>
2022-06-17 00:46:16 +0800

    update

--- bornforthis/.vuepress/navbar.ts
+++ bornforthis/.vuepress/navbar.ts
@@ -14,7 +14,7 @@ export default navbar([
       { 
         text: "程序员的私房菜",
         children: [
-          "/column/cookdinner/01-yogurt"
+          { text: "私房菜", link: "column/cookdinner/01-yogurt"},
         ]
       }
     ]
