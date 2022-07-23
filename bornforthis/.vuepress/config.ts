23d60a4bcd9c1813366e9f5630bdf5591d6f16c5
AndersonHJB <bornforthis@bornforthis.cn>
2022-07-23 11:11:34 +0800

    update

--- bornforthis/.vuepress/config.ts
+++ bornforthis/.vuepress/config.ts
@@ -7,7 +7,7 @@ import theme from "./theme";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "AI悦创",
-  description: "AI悦创,Python,编程一对一,C++,Java,AI,人工智能,黄家宝",
+  description: "AI悦创,一对一,Python,编程一对一,C++,Java,AI,人工智能,黄家宝",
   base: "/",
   shouldPrefetch: false,
   head: [
