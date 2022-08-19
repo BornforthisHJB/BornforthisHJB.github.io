e1fdfc0777bd66cf1580a57628800d4cd3d2a307
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-19 20:54:46 +0800

    update

--- bornforthis/.vuepress/sidebar.ts
+++ bornforthis/.vuepress/sidebar.ts
@@ -31,9 +31,9 @@ import { sidebar } from "vuepress-theme-hope";
 //     });
 
 export default sidebar({
-	"1v1/01-Thomas-hw-note/": [
+	"/1v1/01-Thomas-hw-note/": [
 		"01-Variable",
-		
+		"02-"
 	],
 	"/column/python60/": [
 		"01",
