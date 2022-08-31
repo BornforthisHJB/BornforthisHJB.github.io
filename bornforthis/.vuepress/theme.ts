474380139a8b64ad787484c46dc8c3e895a7e2e6
AndersonHJB <bornforthis@bornforthis.cn>
2022-09-01 01:53:51 +0800

    update

--- bornforthis/.vuepress/theme.ts
+++ bornforthis/.vuepress/theme.ts
@@ -14,7 +14,7 @@ export default hopeTheme({
   iconPrefix: "iconfont icon-",
   
   // iconAssets: "iconfont",
-  iconAssets: "//at.alicdn.com/t/c/font_3426793_sl3kreq6dqo.css",
+  iconAssets: "//at.alicdn.com/t/c/font_3426793_27zl7lu06fo.css",
 
   logo: "/img_home/logo_min.png",
 
