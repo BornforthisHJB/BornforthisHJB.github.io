20a76cd4d756bc75809fa6565994de1267998365
AndersonHJB <cleland1432803776@icloud.com>
2022-09-27 15:18:35 +0000

    Deploying to gh-pages from @ BornforthisHJB/BornforthisHJB.github.io@23014cdceac4b08a956eee7317a16fc149f3b1d4 🚀

---
+++ assets/03-web02.html.cb79d7c3.js
@@ -0,0 +1,50 @@
+import{_ as d}from"./gzh.b03adfd2.js";import{_ as l}from"./zsxq.77f6dc86.js";import{_ as r}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as c,c as t,a as e,b as s,d as n,e as a,r as o}from"./app.819ca3c1.js";const u={},v=e("h2",{id:"_1-vscode-\u5B89\u88C5",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_1-vscode-\u5B89\u88C5","aria-hidden":"true"},"#"),n(" 1. VSCode \u5B89\u88C5")],-1),m=n("\u4E0B\u8F7D\u5730\u5740\uFF1A"),h={href:"https://code.visualstudio.com/",target:"_blank",rel:"noopener noreferrer"},b=n("https://code.visualstudio.com/"),g=a(`<h2 id="_2-\u5B89\u88C5-git\u3001nodejs\u3001yarn" tabindex="-1"><a class="header-anchor" href="#_2-\u5B89\u88C5-git\u3001nodejs\u3001yarn" aria-hidden="true">#</a> 2. \u5B89\u88C5 git\u3001nodejs\u3001yarn</h2><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>brew install git
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>brew install nodejs
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><div class="language-yarn ext-yarn line-numbers-mode"><pre class="language-yarn"><code>sudo npm install --global yarn
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,4),_=e("strong",null,"\u5B89\u88C5\u5931\u8D25\u4F7F\u7528\u5B89\u88C5\u5305\u5B89\u88C5\uFF0C\u4E0B\u8F7D\u5730\u5740",-1),p=n("\uFF1A"),f={href:"https://nodejs.org/en/",target:"_blank",rel:"noopener noreferrer"},x=n("https://nodejs.org/en/"),y=a(`<h2 id="_3-\u68C0\u6D4B-npm-\u7248\u672C" tabindex="-1"><a class="header-anchor" href="#_3-\u68C0\u6D4B-npm-\u7248\u672C" aria-hidden="true">#</a> 3. \u68C0\u6D4B npm \u7248\u672C</h2><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>npm --v
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_4-npm-\u6362\u6E90" tabindex="-1"><a class="header-anchor" href="#_4-npm-\u6362\u6E90" aria-hidden="true">#</a> 4. npm \u6362\u6E90</h2><div class="language-npm ext-npm line-numbers-mode"><pre class="language-npm"><code>npm config set registry https://registry.npm.taobao.org
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_5-git-\u4E0E-github" tabindex="-1"><a class="header-anchor" href="#_5-git-\u4E0E-github" aria-hidden="true">#</a> 5. Git \u4E0E GitHub</h2><div class="language-git ext-git line-numbers-mode"><pre class="language-git"><code>git config --global  --list
+git config --global  user.name <span class="token string">&quot;\u8FD9\u91CC\u6362\u4E0A\u4F60\u7684\u7528\u6237\u540D&quot;</span>
+git config --global user.email <span class="token string">&quot;\u8FD9\u91CC\u6362\u4E0A\u4F60\u7684\u90AE\u7BB1&quot;</span>
+git config --global  --list
+ssh-keygen -t rsa -C <span class="token string">&quot;\u8FD9\u91CC\u6362\u4E0A\u4F60\u7684\u90AE\u7BB1&quot;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_6-\u64CD\u4F5C-github" tabindex="-1"><a class="header-anchor" href="#_6-\u64CD\u4F5C-github" aria-hidden="true">#</a> 6. \u64CD\u4F5C GitHub</h2><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>ssh -T git@github.com 
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_7-\u5B89\u88C5\u7F51\u7AD9" tabindex="-1"><a class="header-anchor" href="#_7-\u5B89\u88C5\u7F51\u7AD9" aria-hidden="true">#</a> 7. \u5B89\u88C5\u7F51\u7AD9</h2><div class="language-git ext-git line-numbers-mode"><pre class="language-git"><code>git clone git@github.com:Ohlalala1021/Ohlalala1021.github.io.git
+npm init vuepress-theme-hope@next [dir]
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>\u535C\u7B97\u5B50\u2022\u548F\u6625\u3010\u56DE\u6587\u8BD7\u3011
+\u4F5C\u8005\uFF1A\u676D\u5DDE\u4E91\u8C37\u5B66\u6821 \u8096\u5F20\u6893\u8F69
+
+\uFF5B\u65E9\u6625\u7BC7\uFF5D
+\u51DB\u51DB\u5BD2\u98CE\u75BE\uFF0C
+\u91D1\u6854\u6620\u6885\u9EC4\uFF0C
+\u843D\u53F6\u677E\u4E0B\u96EA\u7691\u7691\uFF0C
+\u6EF4\u6C34\u5316\u51B0\u68F1\u3002
+
+\u75BE\u98CE\u5BD2\u51DB\u51DB\uFF0C
+\u9EC4\u6885\u6620\u6854\u91D1\uFF0C
+\u7691\u7691\u96EA\u4E0B\u677E\u53F6\u843D\uFF0C
+\u68F1\u51B0\u5316\u6C34\u6EF4\u3002
+
+\uFF5B\u76DB\u6625\u7BC7\uFF5D
+\u6CE5\u6DA6\u6625\u98CE\u8D77\uFF0C
+\u98CE\u821E\u767D\u82B1\u5782\uFF0C
+\u626C\u7D6E\u67F3\u97F5\u4F5C\u98DE\u82B1\uFF0C
+\u6696\u65E5\u866B\u59CB\u6625\u3002
+
+\u8D77\u98CE\u6625\u6DA6\u6CE5\uFF0C
+\u5782\u82B1\u767D\u821E\u98CE\uFF0C
+\u82B1\u98DE\u4F5C\u97F5\u67F3\u7D6E\u626C\uFF0C
+\u6625\u59CB\u866B\u65E5\u6696\u3002
+
+\uFF5B\u63A5\u590F\u7BC7\uFF5D
+\u65E9\u82B1\u5ACC\u6625\u665A\uFF0C
+\u9E23\u866B\u8BED\u82B1\u7E41\uFF0C
+\u7FE0\u67F3\u557C\u83BA\u821E\u8776\u5FD9\uFF0C
+\u98DE\u971E\u604B\u897F\u6EAA\u3002
+
+\u665A\u6625\u5ACC\u82B1\u65E9\uFF0C
+\u7E41\u82B1\u8BED\u866B\u9E23\uFF0C
+\u5FD9\u8776\u821E\u83BA\u557C\u67F3\u7FE0\uFF0C
+\u6EAA\u897F\u604B\u971E\u98DE\u3002\uFFFD
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_8-\u7F51\u7AD9\u542F\u52A8\u547D\u4EE4" tabindex="-1"><a class="header-anchor" href="#_8-\u7F51\u7AD9\u542F\u52A8\u547D\u4EE4" aria-hidden="true">#</a> 8. \u7F51\u7AD9\u542F\u52A8\u547D\u4EE4</h2><div class="language-cmd ext-cmd line-numbers-mode"><pre class="language-cmd"><code>yarn run docs:dev
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="\u5FEB\u6377\u952E" tabindex="-1"><a class="header-anchor" href="#\u5FEB\u6377\u952E" aria-hidden="true">#</a> \u5FEB\u6377\u952E</h2><ol><li>\u5728\u547D\u4EE4\u884C\u4E2D\uFF0C\u952E\u76D8\u5411\u4E0A\u952E\uFF0C\u53EF\u4EE5\u5FEB\u901F\u83B7\u53D6\u4E0A\u4E00\u6761\u547D\u4EE4\u3002</li></ol><h2 id="\u8BFE\u540E\u53CD\u9988" tabindex="-1"><a class="header-anchor" href="#\u8BFE\u540E\u53CD\u9988" aria-hidden="true">#</a> \u8BFE\u540E\u53CD\u9988</h2><ol><li></li></ol><p>\u6B22\u8FCE\u5173\u6CE8\u6211\u516C\u4F17\u53F7\uFF1AAI\u60A6\u521B\uFF0C\u6709\u66F4\u591A\u66F4\u597D\u73A9\u7684\u7B49\u4F60\u53D1\u73B0\uFF01</p><details class="custom-container details"><summary>\u516C\u4F17\u53F7\uFF1AAI\u60A6\u521B\u3010\u4E8C\u7EF4\u7801\u3011</summary><p><img src="`+d+'" alt="" loading="lazy"></p></details>',19),k={class:"custom-container info"},q=e("p",{class:"custom-container-title"},"AI\u60A6\u521B\xB7\u7F16\u7A0B\u4E00\u5BF9\u4E00",-1),j=e("p",null,"AI\u60A6\u521B\xB7\u63A8\u51FA\u8F85\u5BFC\u73ED\u5566\uFF0C\u5305\u62EC\u300CPython \u8BED\u8A00\u8F85\u5BFC\u73ED\u3001C++ \u8F85\u5BFC\u73ED\u3001java \u8F85\u5BFC\u73ED\u3001\u7B97\u6CD5/\u6570\u636E\u7ED3\u6784\u8F85\u5BFC\u73ED\u3001\u5C11\u513F\u7F16\u7A0B\u3001pygame \u6E38\u620F\u5F00\u53D1\u300D\uFF0C\u5168\u90E8\u90FD\u662F\u4E00\u5BF9\u4E00\u6559\u5B66\uFF1A\u4E00\u5BF9\u4E00\u8F85\u5BFC + \u4E00\u5BF9\u4E00\u7B54\u7591 + \u5E03\u7F6E\u4F5C\u4E1A + \u9879\u76EE\u5B9E\u8DF5\u7B49\u3002\u5F53\u7136\uFF0C\u8FD8\u6709\u7EBF\u4E0B\u7EBF\u4E0A\u6444\u5F71\u8BFE\u7A0B\u3001Photoshop\u3001Premiere \u4E00\u5BF9\u4E00\u6559\u5B66\u3001QQ\u3001\u5FAE\u4FE1\u5728\u7EBF\uFF0C\u968F\u65F6\u54CD\u5E94\uFF01\u5FAE\u4FE1\uFF1AJiabcdefh",-1),I=e("p",null,"C++ \u4FE1\u606F\u5965\u8D5B\u9898\u89E3\uFF0C\u957F\u671F\u66F4\u65B0\uFF01\u957F\u671F\u62DB\u6536\u4E00\u5BF9\u4E00\u4E2D\u5C0F\u5B66\u4FE1\u606F\u5965\u8D5B\u96C6\u8BAD\uFF0C\u8386\u7530\u3001\u53A6\u95E8\u5730\u533A\u6709\u673A\u4F1A\u7EBF\u4E0B\u4E0A\u95E8\uFF0C\u5176\u4ED6\u5730\u533A\u7EBF\u4E0A\u3002\u5FAE\u4FE1\uFF1AJiabcdefh",-1),C=n("\u65B9\u6CD5\u4E00\uFF1A"),V={href:"http://wpa.qq.com/msgrd?v=3&uin=1432803776&site=qq&menu=yes",target:"_blank",rel:"noopener noreferrer"},w=n("QQ"),A=e("p",null,"\u65B9\u6CD5\u4E8C\uFF1A\u5FAE\u4FE1\uFF1AJiabcdefh",-1),N=e("p",null,[e("img",{src:l,alt:"",loading:"lazy"})],-1);function Q(B,E){const i=o("ExternalLinkIcon");return c(),t("div",null,[v,e("p",null,[m,e("a",h,[b,s(i)])]),g,e("p",null,[_,p,e("a",f,[x,s(i)])]),y,e("div",k,[q,j,I,e("p",null,[C,e("a",V,[w,s(i)])]),A]),N])}const H=r(u,[["render",Q],["__file","03-web02.html.vue"]]);export{H as default};
