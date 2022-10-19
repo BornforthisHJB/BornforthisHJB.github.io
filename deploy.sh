23e60450bdab787aa3577b5d95ac0bc570d23a91
iMac-Aiyuechuang <1432803776@qq.com>
2022-10-19 19:00:39 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git status
 git add .
 git commit . -m "update"
 git push -u origin main
-git push -f git@121.89.218.11:/home/www/website/ts.git main
+# git push -f git@121.89.218.11:/home/www/website/ts.git main
 # yarn run docs:dev
 
