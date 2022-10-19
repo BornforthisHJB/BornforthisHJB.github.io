97542e183729c2c72309065ef772bd99c4d13f88
AndersonHJB <cleland1432803776@icloud.com>
2022-10-19 11:32:00 +0000

    Deploying to gh-pages from @ BornforthisHJB/BornforthisHJB.github.io@af76c5d956d6d94d0c180c4d98de7589cea7e88e 🚀

---
+++ assets/pageview.1d0a380d.js
@@ -0,0 +1,1 @@
+import{k as e}from"./app.8b1b83e9.js";const a=()=>e({serverURL:"https://comment.bornforthis.cn/"});export{a as updatePageview};
--- assets/pageview.8aae780d.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./app.00dc78d0.js";const a=()=>e({serverURL:"https://comment.bornforthis.cn/"});export{a as updatePageview};
