e4641eb01dd7d00ac521f1dcb53c45774f599eab
iMac-Aiyuechuang <1432803776@qq.com>
2022-10-21 13:18:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ git add .
 git commit . -m "update"
 git push -u origin main
 # 生成静态文件
-# yarn run docs:build
+yarn run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
-git reflog master
+git checkout -b master
 git push -f git@121.89.218.11:/home/www/website/ts.git master
 # git push -f website main
 # yarn run docs:dev
