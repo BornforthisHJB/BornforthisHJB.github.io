eea80a591d2aff85eb085c20409dedd2c2c13af5
iMac-Aiyuechuang <1432803776@qq.com>
2022-10-22 20:41:43 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ git add .
 git commit . -m "update"
 git push -u origin main
 
-yarn run docs:build
-cd docs/.vuepress/dist
+git push -u gitee "master"
 
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@121.89.218.11:/home/www/website/ts.git master
-cd -
+# yarn run docs:build
+# cd docs/.vuepress/dist
+
+# git init
+# git add -A
+# git commit -m 'deploy'
+# git push -f git@121.89.218.11:/home/www/website/ts.git master
+# cd -
