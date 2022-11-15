8003b81640f7294f02b1b63e388ae78772f14782
AndersonHJB <cleland1432803776@icloud.com>
2022-11-15 12:02:44 +0000

    Deploying to gh-pages from @ BornforthisHJB/BornforthisHJB.github.io@fcb6e9f2afc118193dfcbd6893834bb7d3bacb18 🚀

--- assets/pageview.4dbf19f9.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./app.4453f640.js";const i=()=>e({serverURL:"https://comment.bornforthis.cn/"});export{i as updatePageview};
---
+++ assets/skill_pycharm1.html.b0e6c8c3.js
@@ -0,0 +1,41 @@
+import{_ as e}from"./gzh.b03adfd2.js";import{_ as t}from"./zsxq.77f6dc86.js";import{_ as i}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as p,a as n,d as s,b as l,e as c,r}from"./app.ea2aadaf.js";const d="/assets/image-20220520161417800.b6f90188.png",u={},m=c('<h2 id="_1-按照下面路径以此打开" tabindex="-1"><a class="header-anchor" href="#_1-按照下面路径以此打开" aria-hidden="true">#</a> 1. 按照下面路径以此打开</h2><p><img src="'+d+`" alt="image-20220520161417800" loading="lazy"></p><h2 id="_2-设置相关代码" tabindex="-1"><a class="header-anchor" href="#_2-设置相关代码" aria-hidden="true">#</a> 2. 设置相关代码</h2><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token comment">##!/usr/bin/python3</span>
+<span class="token comment"># -*- coding: utf-8 -*-</span>
+<span class="token comment"># @Time    : \${DATE} \${TIME}</span>
+<span class="token comment"># @Author  : AI悦创</span>
+<span class="token comment"># @FileName: \${NAME}.py</span>
+<span class="token comment"># @Software: \${PRODUCT_NAME}</span>
+<span class="token comment"># @Blog    ：https://bornforthis.cn/</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>设定的规则说明如下：</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code>（a）shebang行
+
+<span class="token comment">#!/usr/bin/python3</span>
+
+（b）预定义的变量要扩展为格式为$ <span class="token punctuation">{</span><span class="token operator">&lt;</span>variable_name<span class="token operator">&gt;</span><span class="token punctuation">}</span>的相应值。
+
+可用的预定义文件模板变量为：
+
+$ <span class="token punctuation">{</span>PROJECT_NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 当前项目的名称。
+
+$ <span class="token punctuation">{</span>NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 在文件创建过程中在“新建文件”对话框中指定的新文件的名称。
+
+$ <span class="token punctuation">{</span>USER<span class="token punctuation">}</span> <span class="token operator">-</span> 当前用户的登录名。
+
+$ <span class="token punctuation">{</span>DATE<span class="token punctuation">}</span> <span class="token operator">-</span> 当前的系统日期。
+
+$ <span class="token punctuation">{</span>TIME<span class="token punctuation">}</span> <span class="token operator">-</span> 当前系统时间。
+
+$ <span class="token punctuation">{</span>YEAR<span class="token punctuation">}</span> <span class="token operator">-</span> 今年。
+
+$ <span class="token punctuation">{</span>MONTH<span class="token punctuation">}</span> <span class="token operator">-</span> 当月。
+
+$ <span class="token punctuation">{</span>DAY<span class="token punctuation">}</span> <span class="token operator">-</span> 当月的当天。
+
+$ <span class="token punctuation">{</span>HOUR<span class="token punctuation">}</span> <span class="token operator">-</span> 目前的小时。
+
+$ <span class="token punctuation">{</span>MINUTE<span class="token punctuation">}</span> <span class="token operator">-</span> 当前分钟。
+
+$ <span class="token punctuation">{</span>PRODUCT_NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 将在其中创建文件的IDE的名称。
+
+$ <span class="token punctuation">{</span>MONTH_NAME_SHORT<span class="token punctuation">}</span> <span class="token operator">-</span> 月份名称的前<span class="token number">3</span>个字母。 示例：<span class="token number">1</span>月，<span class="token number">2</span>月等
+
+$ <span class="token punctuation">{</span>MONTH_NAME_FULL<span class="token punctuation">}</span> <span class="token operator">-</span> 一个月的全名。 示例：<span class="token number">1</span>月，<span class="token number">2</span>月等
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>欢迎关注我公众号：AI悦创，有更多更好玩的等你发现！</p><details class="custom-container details"><summary>公众号：AI悦创【二维码】</summary><p><img src="`+e+'" alt="" loading="lazy"></p></details>',8),v={class:"custom-container info"},b=n("p",{class:"custom-container-title"},"AI悦创·编程一对一",-1),k=n("p",null,"AI悦创·推出辅导班啦，包括「Python 语言辅导班、C++ 辅导班、java 辅导班、算法/数据结构辅导班、少儿编程、pygame 游戏开发」，全部都是一对一教学：一对一辅导 + 一对一答疑 + 布置作业 + 项目实践等。当然，还有线下线上摄影课程、Photoshop、Premiere 一对一教学、QQ、微信在线，随时响应！微信：Jiabcdefh",-1),_=n("p",null,"C++ 信息奥赛题解，长期更新！长期招收一对一中小学信息奥赛集训，莆田、厦门地区有机会线下上门，其他地区线上。微信：Jiabcdefh",-1),h={href:"http://wpa.qq.com/msgrd?v=3&uin=1432803776&site=qq&menu=yes",target:"_blank",rel:"noopener noreferrer"},g=n("p",null,"方法二：微信：Jiabcdefh",-1),f=n("p",null,[n("img",{src:t,alt:"",loading:"lazy"})],-1);function y(E,$){const a=r("ExternalLinkIcon");return o(),p("div",null,[m,n("div",v,[b,k,_,n("p",null,[s("方法一："),n("a",h,[s("QQ"),l(a)])]),g]),f])}const I=i(u,[["render",y],["__file","skill_pycharm1.html.vue"]]);export{I as default};
