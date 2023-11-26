e8f2117aec094f87b4209a0b6bd5ec4fb07d6ad2
AndersonHJB <cleland1432803776@icloud.com>
2023-11-26 08:02:27 +0800

    change

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,34 +1,5 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  // "/",
-  "/", // 主页
-  { text: "主页",  icon: "home",  link: "/" },
-
-  {
-    text: "父亲",
-    icon: "C",
-    children: [
-      "/",  // 威力给你体现，还是可以直接存放我们的链接
-      { text: "我是主页儿子", icon: "home", link: "#"},
-      {
-        text: "我是儿子分组",
-        icon: "home",
-        children: [
-          { text: "哈哈哈", icon: "home", link: "#"},
-          { text: "哈哈哈", icon: "home", link: "#"},
-          { text: "哈哈哈", icon: "home", link: "#"},
-          { text: "哈哈哈", icon: "home", link: "#"},
-          { text: "哈哈哈", icon: "home", link: "#"},
-        ]
-      }
-    ]
-  }
-  
-  // {
-  //   text: "C 专栏",
-  //   icon: "c",
-  //   link: "/c-course/01",
-  // }
   
 ]);
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -78,8 +78,8 @@ export default hopeTheme({
     mdEnhance: {
       align: true,
       attrs: true,
-      katex: true,
-      mathjax: false,
+      katex: false,
+      mathjax: true,
 
       // install chart.js before enabling it
       // chart: true,
