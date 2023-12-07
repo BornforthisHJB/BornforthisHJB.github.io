8add1ab3dcbf0fae0eee82ea5926de2f1ce53921
AndersonHJB <cleland1432803776@icloud.com>
2023-12-07 11:45:44 +0800

    04-Python-Logic-Pitfalls-Changing-Lists.md 04-Python-Logic-Pitfalls-Changing-Lists.assets

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,4 +2,5 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   { text: "R-Course", icon: "cib-r", link: "/R-Course/" },
+  { text: "note", icon: "python", link: "/note/" },
 ]);
