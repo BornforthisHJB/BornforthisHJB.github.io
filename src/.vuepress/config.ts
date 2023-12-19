41f6c812f9598cca6befc774e44fa2df0721fe56
AndersonHJB <cleland1432803776@icloud.com>
2023-12-19 15:59:33 +0800

    更新 key

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -15,6 +15,11 @@ export default defineUserConfig({
         href: "/aiyc.svg",
       }
     ],
+    [
+      "meta", {
+        keywords: "一对一辅导编程,编程1v1,CS辅导,编程一对一辅导,少儿编程一对一,Python 1v1,AI悦创,一对一,菜鸟教程,Python,编程一对一,C++,Java,AI,人工智能,黄家宝,Python一对一教学,Python辅导,留学生CS辅导,ChatGPT代注册"
+      }
+    ]
     
     
 
