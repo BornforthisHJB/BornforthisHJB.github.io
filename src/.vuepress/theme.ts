dbda35eec0df8d0848ca0138dc1011a13932b8fc
AndersonHJB <cleland1432803776@icloud.com>
2024-01-14 16:01:40 +0800

    https://docs.bornforthis.cn/Python/Python-core-technology-and-practice/

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -46,7 +46,7 @@ export default hopeTheme({
       "/Python/Assignment/Assignment1.html": ["aiyc", "ydn", "Vector", "yh", "zy", "xxy", "lcy", "kai"],
       "/Python/Assignment/Assignment2.html": ["aiyc", "ydn", "Vector", "yh", "zy", "xxy", "lcy", "kai"],
       "/Python/basequestion/": ["aiyc", "ydn", "Vector", "yh", "zy", "xxy", "lcy", "kai"],
-      "Python/Python-core-technology-and-practice": ["aiyc", "kai"],
+      "/Python/Python-core-technology-and-practice/": ["aiyc", "kai"],
     },
   },
 
