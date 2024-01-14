af326752713b7a8fc6c63e2cb4898c7058f1d3da
AndersonHJB <cleland1432803776@icloud.com>
2024-01-14 14:57:54 +0800

    "Python/Python-core-technology-and-practice"

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,4 +4,35 @@ export default sidebar({
   // "/HomeWork": "structure",
   // "/R-Course/": "structure",
   // "/note/": "structure",
+  "Python/Python-core-technology-and-practice": [
+    {
+      text: "开篇词",
+      collapsible: true,
+      icon: "python",
+      children: [
+        "00",
+      ],
+    },
+    {
+      text: "基础篇",
+      icon: "python",
+      collapsible: true,
+      children: [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+      ],
+    },
+  ]
 });
