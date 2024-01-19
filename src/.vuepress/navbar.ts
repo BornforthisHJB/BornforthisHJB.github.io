1bbfa176e8e10e18c8f7efa941b6baddbeec8a0e
AndersonHJB <cleland1432803776@icloud.com>
2024-01-19 16:36:22 +0800

    有事 有事

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,5 +3,12 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   { text: "R-Course", icon: "cib-r", link: "/R-Course/" },
-  { text: "Python", icon: "python", link: "/Python/" },
+  { 
+    text: "Python", 
+    icon: "python",
+    children: [
+      { text: "All", icon: "python", link: "/Python/" },
+      { text: "NoteBook", icon: "Note", link: "/Python/" },
+    ]
+   },
 ]);
