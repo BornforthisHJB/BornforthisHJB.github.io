3dbde70f1867fb1f7754bdaeb2d2900dadab3109
AndersonHJB <cleland1432803776@icloud.com>
2024-04-19 04:53:34 +0000

    Deploying to gh-pages from @ BornforthisHJB/BornforthisHJB.github.io@78563e29ec26274a8c815147acc801a9e52d0780 🚀

---
+++ assets/skill_pycharm1.html-DKGKNB7y.js
@@ -0,0 +1,41 @@
+import{_ as e}from"./gzh-DnOBNg6W.js";import{_ as t}from"./zsxq-BcdwOI-_.js";import{_ as i}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as l,o,c,a as n,b as s,d as p,e as r}from"./app-CSP3QYYU.js";const d="/assets/image-20220520161417800-xg_O3yPn.png",u={},m=r('<h2 id="_1-按照下面路径以此打开" tabindex="-1"><a class="header-anchor" href="#_1-按照下面路径以此打开"><span>1. 按照下面路径以此打开</span></a></h2><figure><img src="'+d+`" alt="image-20220520161417800" tabindex="0" loading="lazy"><figcaption>image-20220520161417800</figcaption></figure><h2 id="_2-设置相关代码" tabindex="-1"><a class="header-anchor" href="#_2-设置相关代码"><span>2. 设置相关代码</span></a></h2><div class="language-python line-numbers-mode" data-ext="py" data-title="py"><pre class="language-python"><code><span class="token comment">##!/usr/bin/python3</span>
+<span class="token comment"># -*- coding: utf-8 -*-</span>
+<span class="token comment"># @Time    : \${DATE} \${TIME}</span>
+<span class="token comment"># @Author  : AI悦创</span>
+<span class="token comment"># @FileName: \${NAME}.py</span>
+<span class="token comment"># @Software: \${PRODUCT_NAME}</span>
+<span class="token comment"># @Blog    ：https://bornforthis.cn/</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>设定的规则说明如下：</p><div class="language-python line-numbers-mode" data-ext="py" data-title="py"><pre class="language-python"><code>（a）shebang行
+
+<span class="token comment">#!/usr/bin/python3</span>
+
+（b）预定义的变量要扩展为格式为$ <span class="token punctuation">{</span><span class="token operator">&lt;</span>variable_name<span class="token operator">&gt;</span><span class="token punctuation">}</span>的相应值。
+
+可用的预定义文件模板变量为：
+
+$ <span class="token punctuation">{</span>PROJECT_NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 当前项目的名称。
+
+$ <span class="token punctuation">{</span>NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 在文件创建过程中在“新建文件”对话框中指定的新文件的名称。
+
+$ <span class="token punctuation">{</span>USER<span class="token punctuation">}</span> <span class="token operator">-</span> 当前用户的登录名。
+
+$ <span class="token punctuation">{</span>DATE<span class="token punctuation">}</span> <span class="token operator">-</span> 当前的系统日期。
+
+$ <span class="token punctuation">{</span>TIME<span class="token punctuation">}</span> <span class="token operator">-</span> 当前系统时间。
+
+$ <span class="token punctuation">{</span>YEAR<span class="token punctuation">}</span> <span class="token operator">-</span> 今年。
+
+$ <span class="token punctuation">{</span>MONTH<span class="token punctuation">}</span> <span class="token operator">-</span> 当月。
+
+$ <span class="token punctuation">{</span>DAY<span class="token punctuation">}</span> <span class="token operator">-</span> 当月的当天。
+
+$ <span class="token punctuation">{</span>HOUR<span class="token punctuation">}</span> <span class="token operator">-</span> 目前的小时。
+
+$ <span class="token punctuation">{</span>MINUTE<span class="token punctuation">}</span> <span class="token operator">-</span> 当前分钟。
+
+$ <span class="token punctuation">{</span>PRODUCT_NAME<span class="token punctuation">}</span> <span class="token operator">-</span> 将在其中创建文件的IDE的名称。
+
+$ <span class="token punctuation">{</span>MONTH_NAME_SHORT<span class="token punctuation">}</span> <span class="token operator">-</span> 月份名称的前<span class="token number">3</span>个字母。 示例：<span class="token number">1</span>月，<span class="token number">2</span>月等
+
+$ <span class="token punctuation">{</span>MONTH_NAME_FULL<span class="token punctuation">}</span> <span class="token operator">-</span> 一个月的全名。 示例：<span class="token number">1</span>月，<span class="token number">2</span>月等
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>欢迎关注我公众号：AI悦创，有更多更好玩的等你发现！</p><details class="hint-container details"><summary>公众号：AI悦创【二维码】</summary><figure><img src="`+e+'" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure></details>',8),v={class:"hint-container info"},b=n("p",{class:"hint-container-title"},"AI悦创·编程一对一",-1),k=n("p",null,"AI悦创·推出辅导班啦，包括「Python 语言辅导班、C++ 辅导班、java 辅导班、算法/数据结构辅导班、少儿编程、pygame 游戏开发」，全部都是一对一教学：一对一辅导 + 一对一答疑 + 布置作业 + 项目实践等。当然，还有线下线上摄影课程、Photoshop、Premiere 一对一教学、QQ、微信在线，随时响应！微信：Jiabcdefh",-1),h=n("p",null,"C++ 信息奥赛题解，长期更新！长期招收一对一中小学信息奥赛集训，莆田、厦门地区有机会线下上门，其他地区线上。微信：Jiabcdefh",-1),_={href:"http://wpa.qq.com/msgrd?v=3&uin=1432803776&site=qq&menu=yes",target:"_blank",rel:"noopener noreferrer"},g=n("p",null,"方法二：微信：Jiabcdefh",-1),f=n("figure",null,[n("img",{src:t,alt:"",tabindex:"0",loading:"lazy"}),n("figcaption")],-1);function y(A,T){const a=l("ExternalLinkIcon");return o(),c("div",null,[m,n("div",v,[b,k,h,n("p",null,[s("方法一："),n("a",_,[s("QQ"),p(a)])]),g]),f])}const I=i(u,[["render",y],["__file","skill_pycharm1.html.vue"]]),M=JSON.parse('{"path":"/Python/basequestion/skill_pycharm1.html","title":"01-Pycharm 自动添加作者等信息","lang":"zh-CN","frontmatter":{"title":"01-Pycharm 自动添加作者等信息","date":"2022-05-20T16:01:00.000Z","author":"AI悦创","isOriginal":true,"category":"编辑器技巧","tag":["编辑器技巧","Pycharm","使用技巧"],"icon":"python","sticky":false,"star":false,"article":true,"timeline":true,"image":false,"navbar":true,"sidebarIcon":true,"headerDepth":5,"comment":true,"lastUpdated":true,"editLink":true,"prev":false,"next":"skill_ipython1.md","backToTop":true,"toc":true,"feed":false,"seo":false,"head":[]},"headers":[{"level":2,"title":"1. 按照下面路径以此打开","slug":"_1-按照下面路径以此打开","link":"#_1-按照下面路径以此打开","children":[]},{"level":2,"title":"2. 设置相关代码","slug":"_2-设置相关代码","link":"#_2-设置相关代码","children":[]}],"git":{"createdTime":1704501196000,"updatedTime":1704501196000,"contributors":[{"name":"AndersonHJB","email":"cleland1432803776@icloud.com","commits":1}]},"readingTime":{"minutes":1.68,"words":505},"filePathRelative":"Python/basequestion/skill_pycharm1.md","localizedDate":"2022年5月20日","copyright":{"author":"AI悦创"}}');export{I as comp,M as data};
